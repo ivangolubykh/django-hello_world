34aeeee41310bb48647b6b07fcf70b1e2a13c476
ivangolubykh <ivan9@allworld.xyz>
2016-12-19 14:21:23 +0300

    Синтаксическая правка, сократил 1 строку кода во вьюшке.

--- django_01/indexapp/views.py
+++ django_01/indexapp/views.py
@@ -20,6 +20,5 @@ def org_card(request, num):
     return render_to_response("org_card.html", {'org_places': org_places})
 
 def work(request):
-    work_places_first = Work.objects.values('date_start', 'date_end', 'position', 'descr', 'organization', 'organization__name', 'organization__region', 'organization__address').order_by('-date_start')[:3]
-    work_places_last = Work.objects.values('date_start', 'date_end', 'position', 'descr', 'organization', 'organization__name', 'organization__region', 'organization__address').order_by('-date_start')[3:]
-    return render_to_response("work.html", {'work_places_first': work_places_first, 'work_places_last': work_places_last})
+    work_places = Work.objects.values('date_start', 'date_end', 'position', 'descr', 'organization', 'organization__name', 'organization__region', 'organization__address').order_by('-date_start')
+    return render_to_response("work.html", {'work_places_first': work_places[:3], 'work_places_last': work_places[3:]})
